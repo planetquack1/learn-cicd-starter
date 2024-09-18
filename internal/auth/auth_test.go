ec8b06af08b7fbb91336954d00f290cc5ffe42e6
Isaiah <irodol@uw.edu>
2024-09-18 15:52:34 -0700

    Force fail new tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+
+	header := http.Header{}
+	header.Set("Authorization", "ApiKey sampleKey123")
+
+	apiKey, err := GetAPIKey(header)
+
+	if apiKey != "sampleKey789" {
+		t.Errorf("expected 'ApiKey sampleKey123', got '%s'", apiKey)
+	}
+
+	if err != nil {
+		t.Errorf("error getting ApiKey %v", err)
+	}
+
+}
